54e9d85ba041a5eae1a00a310c01b2d11e8d94cf
Faisal Ramadhan <faisalramadhan1299@gmail.com>
2023-12-29 19:13:10 +0700

    fix several issues

---
+++ app/register/page.js
@@ -0,0 +1,10 @@
+export default function Register() {
+    return (
+      <main>
+        <div className='flex align-middle items-center justify-center'>
+          <h1 className='bg-yellow-400 text-gray-900 font-semibold p-4'>Welcome to Register page!</h1>
+        </div>
+      </main>
+    )
+  }
+  
